4e20d4fcf652159d61d9d0d2c474bd27d01a761c
ztipsamme <e.spitz@hotmail.com>
2023-12-11 03:39:20 +0100

    jbvjda

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/deploying-jontastic-slots2/',
+  base: '/jontastic-slots2/',
   plugins: [vue()],
 })
